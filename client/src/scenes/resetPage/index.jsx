caef9625426a0d1fe814d271bbb2459627d14cb7
Shubh-81 <shubh57ag@gmail.com>
2023-11-17 16:44:39 +0530

    Smatching to IITGN

--- client/src/scenes/resetPage/index.jsx
+++ client/src/scenes/resetPage/index.jsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
         textAlign="center"
       >
         <Typography fontWeight="bold" fontSize="32px" color="primary">
-          SMatching
+        IIT Gandhinagar Transport Management
         </Typography>
       </Box>
 
